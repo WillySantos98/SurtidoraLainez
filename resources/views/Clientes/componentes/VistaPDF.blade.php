327703b7e4f20ca6f7dbe79f5dda672722c754c1
WillySantos98 <willysantos_lai@yahoo.com>
2019-08-31 17:32:36 -0600

    Errores 0.1.1 documentos clientes

--- resources/views/Clientes/componentes/VistaPDF.blade.php
+++ resources/views/Clientes/componentes/VistaPDF.blade.php
@@ -7,7 +7,7 @@
     <tbody>
     @foreach($datosDocumentos as $doc)
         <tr>
-            <td><a href="{{asset('/documentos/clienes/'.$doc->nombre)}}" target="_blank">{{$doc->nombre}}</a></td>
+            <td><a href="public/documentos/clientes/{{$doc->nombre}}" target="_blank">{{$doc->nombre}}</a></td>
         </tr>
     @endforeach
     </tbody>
